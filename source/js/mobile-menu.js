60baf3d09a84431fc304c0cfc7b2c18744a3b47c
AndrewPlatonov888 <very@blacky.ru>
2023-09-28 12:09:21 +0300

    ДЗ module5-task1 - детали , мелкие элементы..

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,9 +1,18 @@
 const mobileMenuBtn = document.querySelector('.main-header__toggle');
 const mobileMenu = document.querySelector('.site-list');
-const mediaQuery = window.matchMedia('(min-width: 768px)')
+const mainContainer = document.querySelector('.main-container');
 
 
-mobileMenuBtn.addEventListener('click', ToggleFunction);
+
+const mouseWatcher = function(evt) {
+  // console.log('функция сработала');
+  // if (!mobileMenu.contains(evt.target) && !mobileMenuBtn.contains(evt.target)) {
+    evt.stopPropagation();
+    mobileMenu.style.display = 'none';
+    mobileMenuBtn.value = 'off'
+    mobileMenuBtn.style.backgroundImage = 'url("../../img/toggle_disabled.svg")';
+  // }
+}
 
 function ToggleFunction() {
   if(mobileMenuBtn.value == 'off'){
@@ -18,10 +27,34 @@ function ToggleFunction() {
 }
 }
 
-//  // закрытие элемента по клику вне элемента
-//  document.addEventListener("mouseup", function (evt) {
-//   if (!mobileMenu.contains(evt.target) && !mobileMenuBtn.contains(evt.target)) {
-//     mobileMenu.style.display = 'none';
-//     mobileMenuBtn.style.backgroundImage = 'url("../../img/toggle_disabled.svg")';
-//   }
-// });
+
+// Используем ResizeObserver для отслеживания resize (ps^объект window не работает в node.js - проверено :(  )
+const ro = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    const cr = entry.contentRect;
+    console.log('Element:', entry.target);
+    console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+    console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+
+    if (cr.width > 0) {
+      console.log('ширина больше нуля: 1. запустить листенер на клик 2. запустить листенер на мауслив 3. сделать дисплей ноне на меню')
+      mobileMenuBtn.value = 'off'
+  mobileMenu.style.display = 'none';
+      mobileMenuBtn.addEventListener('click', ToggleFunction);
+      mainContainer.addEventListener('touchstart', mouseWatcher);
+      mobileMenu.addEventListener('mouseleave', mouseWatcher);
+    }
+    else {
+      console.log('ширина равна нулю: 1. убрать листенер на клик 2. убрать листенер на мауслив 3. сделать дисплей флекс на меню')
+      mobileMenuBtn.value = 'on'
+  mobileMenu.style.display = 'flex';
+      mobileMenuBtn.removeEventListener('click', ToggleFunction);
+      mainContainer.removeEventListener('touchstart', mouseWatcher);
+      mobileMenu.removeEventListener('mouseleave', mouseWatcher);
+    }
+  }
+});
+
+// Cледим за наличием кнопки мобильного меню
+ro.observe(mobileMenuBtn);
+
